b32929866ae3675640681fe4248a924d51200a3e
michelle nderi <nderimichelle@gmail.com>
2023-06-21 21:43:09 +0300

    Complete assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"], message: "is not included in the list" }
+    validate :clickbait_title
+  
+    private
+  
+    def clickbait_title
+      clickbait_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+      if title.present? && !clickbait_keywords.any? { |keyword| title.include?(keyword) }
+        errors.add(:title, "should be clickbait-y")
+      end
+    end
+  end
+  
